48e9bb5516fc731800b5733eeb00de9f833a0a90
DenysVoloshchuk <voloshchuk003@gmail.com>
2022-11-02 16:47:32 +0200

    1.4
    Створено найпростішу чат-кімнату

--- Lab1/client.py
+++ Lab1/client.py
@@ -1,14 +1,51 @@
-from ctypes import sizeof
+from datetime import datetime
 import socket
+import threading
 
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.connect((socket.gethostname(), 1309))
+nickname = input('Enter a nickname: ')
 
-while True:
-    msg = input("Text your massage to server: ")
-    if msg == 'stop':
-        serv.send(msg.encode())
-        break
-    serv.send(msg.encode())
-    msg = serv.recv(1024)
-    print(msg.decode('utf-8'))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((socket.gethostname(), 1309))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        text = input('')
+        if text.lower == 'exit':
+            client.close()
+            break
+        message = f'{datetime.now()} | {nickname}: {text}'
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+
+
+
+
+
+
+# while True:
+#     msg = input("Text your massage to server: ")
+#     if msg == 'stop':
+#         client.send(msg.encode())
+#         break
+#     client.send(msg.encode())
+#     msg = client.recv(1024)
+#     print(msg.decode('utf-8'))
--- Lab1/server.py
+++ Lab1/server.py
@@ -1,24 +1,49 @@
 import socket
-from datetime import datetime
-from time import sleep
+import threading
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind((socket.gethostname(), 1309))
 
 serv.listen()
 
-clientsocket, address = serv.accept()
-while True:
-    msg = clientsocket.recv(1024)
-    if msg.decode('utf-8') == 'stop':
-        break    
-    print(datetime.now(), ' > ', msg.decode('utf-8'))
+clients = []
+nicknames = []
 
-    sleep(5)
-    if  len(msg.decode('utf-8')) == len(msg):
-        clientsocket.send((msg))
-        print("All data sented succesfully")
-    else:
-        print('Error')
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames [index]
+            broadcast(f' {nickname} left the chat!' .encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = serv.accept()
+        print(f"connected with {str(address)}")
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        client.send(f'Connected to the server!'.encode('ascii'))
+        if len(clients) == 0:
+            serv.close()
+            break
+        thread = threading.Thread(target=handle, args=(client, ))
+        thread.start()
+
+print('Server has started')
+receive()
 
